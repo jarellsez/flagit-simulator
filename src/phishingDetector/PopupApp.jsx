f9ee2c4b7b2dd33a78ee4ed49380e229556762e3
jarellsez <jarell.emilie365@gmail.com>
2026-02-24 16:09:05 +0800

    Start Screen Extension Complete

--- src/phishingDetector/PopupApp.jsx
+++ src/phishingDetector/PopupApp.jsx
@@ -1,34 +1,15 @@
 import React from "react";
+import DetectorContainer from "./components/DetectorContainer";
 import "./styles/popup.css";
 
 export default function PopupApp() {
   return (
-    <div style={{ width: "100%", height: "100%", padding: 16, color: "white" }}>
-      <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
-        <div
-          style={{
-            width: 40,
-            height: 40,
-            borderRadius: 10,
-            background: "#0b2a55",
-          }}
-        />
-        <div>
-          <div style={{ fontWeight: 700 }}>FlagIt</div>
-          <div style={{ opacity: 0.8, fontSize: 12 }}>Phishing Detector</div>
-        </div>
-      </div>
-
-      <div
-        style={{
-          marginTop: 18,
-          padding: 14,
-          borderRadius: 14,
-          background: "#0b2a55",
-        }}
-      >
-        Popup is rendering correctly ✅
-      </div>
+    <div style={{ 
+      width: "400px", 
+      minHeight: "500px",
+      backgroundColor: "#0a1a2f",  // This is your dark blue
+    }}>
+      <DetectorContainer />
     </div>
   );
 }
